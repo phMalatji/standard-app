20b94afebcc578671c2c3fad38a594fc5a841aed
PSK <rmsmalatji@outlook.com>
2022-06-29 14:01:57 +0200

    init apps

---
+++ server.mjs
@@ -0,0 +1,30 @@
+'use strict';
+
+import express from 'express';
+ import fetch from 'node-fetch';
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/poke', function (req, res) {
+    var url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        res.send({ data });
+    })
+    .catch(err => {
+        res.send(err);
+    });
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
