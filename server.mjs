d0897adeb5d66a99eca3e98e626f5d28a94959cb
PSK <rmsmalatji@outlook.com>
2022-06-29 14:56:53 +0200

    add cors

--- server.mjs
+++ server.mjs
@@ -2,6 +2,7 @@
 
 import express from 'express';
  import fetch from 'node-fetch';
+ import cors from 'cors';
 
 // Constants
 const PORT = 8080;
@@ -9,12 +10,15 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.get('/poke', function (req, res) {
-    var url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+    var url = 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0';
     
     fetch(url)
     .then(res => res.json())
